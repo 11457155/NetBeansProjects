a1f697d23d446b7f705615503a64ccf77c23ef8d
BIN LI <anddicli@gmail.com>
2013-01-26 22:48:48 +0800

    output each word each line

---
+++ 1-5/main.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   main.cpp
+ * Author: libin
+ *
+ * Created on January 26, 2013, 10:16 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+
+/* input a variety of data */
+void outputRegulate()
+{
+    cout << "the original output is: " << endl;
+    cout << "starting outputting data using this way" << endl;
+    cout << endl;
+    cout << "the formatted output is: "<< endl;
+    cout << "start" << endl\
+         << "data" << endl\
+         << "transfer" <<endl\
+         << "using" <<endl\
+         << "this" <<endl\
+         << "way" <<endl;
+     
+}
+
+int main(int argc, char** argv) {
+
+    outputRegulate();
+    return 0;
+}
+
