05647ebabd6862751da1bad2cc4fe1ee61522608
BIN LI <anddicli@gmail.com>
2013-01-27 16:10:07 +0800

    add between user's input

---
+++ 1-15/main.cpp
@@ -0,0 +1,50 @@
+/* 
+ * File:   main.cpp
+ * Author: libin
+ *
+ * Created on January 27, 2013, 3:52 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+
+int addFunction()
+{
+    cout << "now starting add, please let the user input the desired data:" << endl;
+    int inputVal1, inputVal2;
+    int sum = 0;
+    int lower, higher;
+    
+    cin >> inputVal1 >> inputVal2;
+    
+    if(inputVal1 > inputVal2)
+    {
+        lower = inputVal2;
+        higher = inputVal1;
+    }
+    
+    else if(inputVal1 < inputVal2)
+    {
+        lower = inputVal1;
+        higher = inputVal2;
+    }
+    
+    for(int i = lower; i <= higher; ++i)
+    {
+        sum += i;
+    }
+    
+    return sum;
+}
+int main(int argc, char** argv) {
+
+    cout << "the add result is:" << addFunction << endl;
+    return 0;
+}
+
